9ce0d61a1057de4136f5d8df4c1a1df696b0a0f4
waris aremou <warisaremou2003@gmail.com>
2024-03-24 16:07:19 +0100

    Update UI component sizes

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
 			variant: {
 				default: "bg-primary text-primary-foreground hover:bg-primary/90",
 				destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-				outline: "border border-primary bg-background hover:bg-background text-primary",
+				outline: "border border-primary bg-background text-primary hover:bg-background",
 				secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
 				ghost: "hover:bg-accent hover:text-accent-foreground",
 				link: "text-primary underline-offset-4 hover:underline",
@@ -21,7 +21,7 @@ const buttonVariants = cva(
 				default: "h-10 px-4 py-2",
 				sm: "h-9 rounded-md px-3",
 				lg: "h-11 rounded-md px-8",
-				icon: "h-10 w-10",
+				icon: "size-10",
 			},
 		},
 		defaultVariants: {
