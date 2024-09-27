cc453931aa18d1b93a740c9e3013f7e2a896c189
uhoungbo <h.ulrich@softvodooz.com>
2024-09-27 11:09:46 +0100

    fix: remove buy credit page

--- app/dashboard/sms/send-sms/page.tsx
+++ app/dashboard/sms/send-sms/page.tsx
@@ -1,26 +1,25 @@
 import { Breadcrumbs } from "@/components/pagers/breadcrumbs";
-import SendSmsForm from "@/components/forms/send-sms-form";
 import { routes } from "@/lib/routes";
 import { Metadata } from "next";
 import AddProxyForm from "@/components/forms/add-proxy-form";
 
 export const metadata: Metadata = {
-	title: "Dashboard - Send Sms",
-	description: "Dashboard send sms page",
+  title: "Dashboard - Send Sms",
+  description: "Dashboard send sms page",
 };
 
 export default function SendSmsPage() {
-	return (
-		<div className="space-y-5">
-			<Breadcrumbs
-				segments={[
-					{ title: "Proxy", href: routes.dashboard.sms.index },
-					{ title: "Ajouter un proxy", href: routes.dashboard.sms.send },
-				]}
-			/>
-			<div className="w-full">
-				<AddProxyForm />
-			</div>
-		</div>
-	);
+  return (
+    <div className="space-y-5">
+      <Breadcrumbs
+        segments={[
+          { title: "Proxy", href: routes.dashboard.sms.index },
+          { title: "Ajouter un proxy", href: routes.dashboard.sms.send },
+        ]}
+      />
+      <div className="w-full">
+        <AddProxyForm mode="add" />
+      </div>
+    </div>
+  );
 }
