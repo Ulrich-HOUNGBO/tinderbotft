0701ecdec343b2a1fb5611f1d474b293bd05844a
Ulrich <uhoungbo@gmail.com>
2024-11-21 14:37:49 +0100

    fix: Add user session and configuration to sidebar
    This commit integrates user session with `next-auth` and custom user hooks into the mobile dashboard sidebar component. Conditional navigation configuration is applied based on user roles.

--- components/layouts/mobile-dashboard-sidebar.tsx
+++ components/layouts/mobile-dashboard-sidebar.tsx
@@ -8,34 +8,42 @@ import { Button } from "../ui/button";
 import { ScrollArea } from "../ui/scroll-area";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import DashboardSidebarSharedContent from "./dashboard-sidebar-shared-content";
+import { useSession } from "next-auth/react";
+import { useMe } from "@/services/users/hooks";
 
 export default function MobileDashboardSidebar() {
-	const pathname = usePathname();
-	const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+  const { data: session } = useSession();
+  const { data: user, isLoading, error } = useMe(!!session?.accessToken);
 
-	return (
-		<Sheet open={isOpen} onOpenChange={setIsOpen}>
-			<SheetTrigger asChild>
-				<Button
-					aria-label="Toggle Menu"
-					variant="ghost"
-					onClick={() => setIsOpen(false)}
-					className="border px-2 text-base text-secondary-foreground hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 lg:hidden"
-				>
-					<Menu className="size-6" />
-					<span className="sr-only">Toggle Menu</span>
-				</Button>
-			</SheetTrigger>
+  const config = user
+    ? dashboardConfig({ super_user: user.is_superuser })
+    : { mainNav: [] };
 
-			<SheetContent side="mobileLeft" className="px-0 lg:hidden">
-				<ScrollArea className="my-4 h-[calc(100vh-8rem)] px-6 pb-10">
-					<DashboardSidebarSharedContent
-						sidebarNavItems={dashboardConfig.mainNav}
-						pathname={pathname}
-						setIsOpen={setIsOpen}
-					/>
-				</ScrollArea>
-			</SheetContent>
-		</Sheet>
-	);
+  return (
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger asChild>
+        <Button
+          aria-label="Toggle Menu"
+          variant="ghost"
+          onClick={() => setIsOpen(false)}
+          className="border px-2 text-base text-secondary-foreground hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 lg:hidden"
+        >
+          <Menu className="size-6" />
+          <span className="sr-only">Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+
+      <SheetContent side="mobileLeft" className="px-0 lg:hidden">
+        <ScrollArea className="my-4 h-[calc(100vh-8rem)] px-6 pb-10">
+          <DashboardSidebarSharedContent
+            sidebarNavItems={config.mainNav}
+            pathname={pathname}
+            setIsOpen={setIsOpen}
+          />
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
 }
