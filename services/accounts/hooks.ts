e8e680657c4bf311e84727bcb2eb3d6987423744
Ulrich <uhoungbo@gmail.com>
2024-12-03 12:56:02 +0100

    feat: Add reset password query

--- services/accounts/hooks.ts
+++ services/accounts/hooks.ts
@@ -1,35 +1,45 @@
-import { confirmEmail, forgotPassword, resetPassword } from "@/services/accounts/queries";
+import {
+  confirmEmail,
+  forgotPassword,
+  resetPassword,
+} from "@/services/accounts/queries";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 // --------------- QUERY & MUTATION KEYS --------------- //
 export const accountsQueryKeys = {
-	confirmEmailKey: ["confirm-email"],
-	forgotPasswordKey: ["forgot-password"],
-	resetPasswordKey: ["reset-password"],
+  confirmEmailKey: ["confirm-email"],
+  forgotPasswordKey: ["forgot-password"],
+  resetPasswordKey: ["reset-password"],
 };
 
 // --------------- QUERIES HOOKS --------------- //
 export const useConfirmEmail = (token: string) => {
-	return useQuery({
-		queryKey: accountsQueryKeys.confirmEmailKey,
-		queryFn: async () => {
-			return await confirmEmail(token);
-		},
-		enabled: !!token,
-	});
+  return useQuery({
+    queryKey: accountsQueryKeys.confirmEmailKey,
+    queryFn: async () => {
+      return await confirmEmail(token);
+    },
+    enabled: !!token,
+  });
 };
 
 // --------------- MUTATIONS HOOKS --------------- //
 export const useForgotPassword = () => {
-	return useMutation({
-		mutationKey: accountsQueryKeys.forgotPasswordKey,
-		mutationFn: (email: string) => forgotPassword(email),
-	});
+  return useMutation({
+    mutationKey: accountsQueryKeys.forgotPasswordKey,
+    mutationFn: (email: string) => forgotPassword(email),
+  });
 };
 
 export const useResetPassword = (token: string) => {
-	return useMutation({
-		mutationKey: accountsQueryKeys.resetPasswordKey,
-		mutationFn: (newPassword: string) => resetPassword(token, newPassword),
-	});
+  return useMutation({
+    mutationKey: accountsQueryKeys.resetPasswordKey,
+    mutationFn: ({
+      newPassword,
+      confirmPassword,
+    }: {
+      newPassword: string;
+      confirmPassword: string;
+    }) => resetPassword(token, newPassword, confirmPassword),
+  });
 };
