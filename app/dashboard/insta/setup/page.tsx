a69fa19d8d2223a579ad637ce3b8981af3ca047d
Ulrich <uhoungbo@gmail.com>
2025-02-21 09:29:37 +0100

    feat: create Insta connection page with metadata and breadcrumbs

---
+++ app/dashboard/insta/setup/page.tsx
@@ -0,0 +1,22 @@
+import {Metadata} from "next";
+import {Breadcrumbs} from "@/components/pagers/breadcrumbs";
+import {Suspense} from "react";
+
+export const metadata: Metadata = {
+    title: "Setup - Insta",
+    description: "Setup insta page",
+};
+
+export default function SetupPage() {
+    return (
+        <div className="space-y-5">
+            <div className="flex items-center justify-between">
+                <Breadcrumbs segments={[{title: "Setup"}]}/>
+            </div>
+
+            <Suspense>
+                <p>connect  page</p>
+            </Suspense>
+        </div>
+    );
+}
